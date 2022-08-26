42397363f6e98d251e8b353974a4a518c64a223c
elinarnten <elin.arnten@hotmail.com>
2022-08-26 14:46:20 +0200

    try to merge

--- src/assets/teamData.js
+++ src/assets/teamData.js
@@ -13,7 +13,7 @@ export const team = [
     github: "https://github.com/Prisberg",
     linkedin: "https://www.linkedin.com/in/philip-risberg/",
   },
-  {},
+
   {
     name: "Elin Arntén",
     image: `${elin}`,
@@ -21,10 +21,11 @@ export const team = [
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
        Duisaute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+
     github: "",
     linkedin: "",
   },
-  {},
+
   {
     name: "Hazem Kawas",
     image: "./images./hazem.png",
@@ -35,7 +36,7 @@ export const team = [
     github: "",
     linkedin: "",
   },
-  {},
+
   {
     name: "Erik Matsgård",
     image: `${erik}`,
@@ -48,7 +49,7 @@ export const team = [
     github: "",
     linkedin: "",
   },
-  {},
+
   {
     name: "Isaaq Guleed",
     image: "./images./isaaq.png",
