a8af93bf3e8931b1eebfdbc364a42469ca57cb45
Hazem Kawas <hazem.k.4488@gmail.com>
2022-08-29 10:05:28 +0200

    mobile style for each skill page

--- src/components/main/skills/skills-Info/Figma.jsx
+++ src/components/main/skills/skills-Info/Figma.jsx
@@ -8,14 +8,14 @@ import './skillInfo.css'
 
 const Figma = () => {
   return (
-    <Box sx={mainBox}>
+    <Box sx={mainBox} className='main-Box'>
       <Box sx={innerBox}>
         <Box>
         <Typography sx={mainTitle}>
           Figma
           </Typography>
         </Box>
-        <Box sx={skillProjects}>
+        <Box sx={skillProjects} className='skillProjects'>
           <Box sx={projectBox}  className='skillBox'>
           <img src={skillTest} alt=""/>
           </Box>
@@ -30,15 +30,15 @@ const Figma = () => {
           </Box>
         </Box>
         <Box sx={skillInfo}>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        <Typography sx={skillInfoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Nam hic blanditiis tenetur delectus voluptatibus facilis aliquam vitae corrupti modi saepe consequuntur, 
             accusamus ducimus, natus enim impedit, sit at cum. Qui nulla quidem aspernatur maiores tenetur temporibus magnam amet! Sint quibusdam corporis ab 
             exercitationem alias eaque aliquid porro error ullam sequi?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Nam hic blanditiis tenetur delectus voluptatibus facilis aliquam vitae corrupti modi saepe consequuntur, 
             accusamus ducimus, natus enim impedit, sit at cum. Qui nulla quidem aspernatur maiores tenetur temporibus magnam amet! Sint quibusdam corporis ab 
             exercitationem alias eaque aliquid porro error ullam sequi?
-          </p>
+          </Typography>
         </Box>
       </Box>
     </Box>
@@ -46,41 +46,50 @@ const Figma = () => {
 }
 const mainBox = {
   width:{ xs: '100%', md: '100%', lg: '100%', xl: '100%'},
-  height:{ xs: '100%', md: '100%', llg: '100%', xl: '1500px'},
+  height:{ xs: '100%', md: '100%', llg: '100%', xl: '1300px'},
   backgroundColor: '#EDE6E3',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 }
 const innerBox = {
-  width:{ xs: '100%', md: '100%', lg: '1226px', xl: '70%'},
-  height:{ xs: '100%', md: '100%', llg: '100%', xl: '1350px'},
+  width:{ xs: '80%', md: '100%', lg: '1226px', xl: '70%'},
+  height:{ xs: '80%', md: '100%', lg: '100%', xl: '1150px'},
   padding: '2em',
+  marginTop: { xs: '2em', md: '0', lg: '0', xl: '0'},
   backgroundColor: '#F9F7F7',
-  paddingLeft: '15em',
-  // borderRadius: '2em',
+  paddingLeft: { xs: '0em', md: '15em', lg: '15em', xl: '15em'},
+  borderRadius: '2em',
 }
 const skillProjects = {
-  width:{ xs: '100%', md: '500px', lg: '500px', xl: '1226px'},
   display: 'flex',
-  alignItems: 'flex-start',
+  alignItems:{ xs: 'center', md: 'flex-start', lg: 'flex-start', xl: 'flex-start'},
   justifyContent: 'center',
-  flexWrap: 'wrap',
+  width:{ xs: '380px', s: '380px', md: '500px', lg: '1226px', xl: '1226px'},
+  flexWrap: { xs: 'no-wrap', md: 'wrap', lg: 'wrap', xl: 'wrap'},
   marginTop: '2em',
+  flexDirection: { xs: 'column', md: 'row', lg: 'row'}
 }
 const  projectBox = {
  flex: '50%',
  marginTop: '2em',
- width: '300px',
+ width:{ xs: '200px', md: '300px', lg: '300px', xl: '300px'},
+ borderRadius: '2em',
 }
 const skillInfo = {
-  width:{ xs: '', md: '', lg: '70%', xl: '85%'},
-  minHeight:{ xs: '1', md: '', llg: '100%', xl: '400px'},
-  fontSize: '1.5em '
+  width:{ xs: '100%', md: '500px', lg: '500px', xl: '970px'},
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginTop: '1em',
 }
 const mainTitle = {
-  textAlign: 'left',
+  textAlign: { xs: 'center', md: 'left', lg: 'left', xl:'left'},
   fontSize: { xs: '2em', md: '2em', lg: '2em', xl: '4em'},
+}
+const skillInfoText = {
+  fontSize: '1.3em',
+  paddingLeft: {xs: '20px', md: '0px', lg: '0px', xl: '0px'}
 }
 
 
