92c4ec728b375afd916cff3ad02c0977e8dd1436
Prisberg <fille.risberg@live.se>
2022-08-30 19:41:02 +0200

    extract from localstorage function

--- src/utils/PortfolioContext.jsx
+++ src/utils/PortfolioContext.jsx
@@ -1,23 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const PortfolioContext = createContext({
     username: '',
     setUsername: () => false,
     password: '',
     setPassword: () => false,
+    extractLocalStorage: () => false,
 })
 
 export default function PortfolioProvider(props) {
+    const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
-    const [password, setPassword] = useState(false);
+    // const [lName, setLName] = useState('');
+    const [password, setPassword] = useState('');
+
+    function extractLocalStorage() {
+        //Function for getting localstorage data. To be used when account created and on first load.
+        setEmail(localStorage.getItem('email').replace(/['"]+/g, ''));
+        setUsername(localStorage.getItem('fName').replace(/['"]+/g, ''));
+        // setLName(localStorage.getItem('lName').replace(/['"]+/g, '')); Use this to autofill email form?
+        setPassword(localStorage.getItem('password').replace(/['"]+/g, ''));
+    }
+
+    useEffect(() => {
+        extractLocalStorage();
+    }, [])
 
     return (
         <PortfolioContext.Provider
             value={{
+                email,
                 username,
-                setUsername,
                 password,
-                setPassword
+                extractLocalStorage
             }}
             {...props} />
     )
