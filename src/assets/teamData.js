db18edc0bd834d032d789302cf4513a8754f9ddd
rchtect <rchtectdev@gmail.com>
2022-08-30 13:03:04 +0200

    fix merge issues

--- src/assets/teamData.js
+++ src/assets/teamData.js
@@ -1,8 +1,9 @@
-import philip from "./images/philip.png";
-import erik from "./images/erik.png";
-import hazem from "./images/hazem.jpg";
+
+import philip from "./images/philip.png"
+import erik from "./images/erik.png"
+import hazem from "./images/hazem.jpg"
 import elin from "./images/elin.png";
-import isaaq from "./images/isaaq.jpg";
+
 
 export const team = [
   {
@@ -28,14 +29,15 @@ export const team = [
   },
 
   {
-    name: "Hazem Kawas",
+    name: 'Hazem Kawas',
     image: `${hazem}`,
     about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
        Duisaute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-    github: "https://github.com/hazem-89",
-    linkedin: "https://www.linkedin.com/in/hazem-kawas-66971b185/",
+    github: 'https://github.com/hazem-89',
+    linkedin: 'https://www.linkedin.com/in/hazem-kawas-66971b185/'
+
   },
 
   {
@@ -53,12 +55,12 @@ export const team = [
 
   {
     name: "Isaaq Guleed",
-    image: `${isaaq}`,
+    image: "./images./isaaq.png",
     about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
        Duisaute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-    github: "https://rchtect.github.io",
-    linkedin: "https://se.linkedin.com/in/isaaq-guleed-6a0614239",
+    github: "",
+    linkedin: "",
   },
 ];
