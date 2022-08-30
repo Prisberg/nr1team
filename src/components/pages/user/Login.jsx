0a9bf518bcc1a44a1f1b12ffecba42067b8268cb
Prisberg <fille.risberg@live.se>
2022-08-30 08:49:44 +0200

    header and responsiveness

--- src/components/pages/user/Login.jsx
+++ src/components/pages/user/Login.jsx
@@ -8,37 +8,46 @@ function Login() {
             autoComplete="off"
             /* onSubmit={handleSubmit} */>
             <Box sx={loginMainBox}>
-                <TextField
-                    /* onChange={handleInputValue} */
-                    /*   onBlur={handleInputValue} */
-                    name="email"
-                    label="Email"
-                    fullWidth
-                    autoComplete
-                    required
-                />
-                <TextField
-                    /* onChange={handleInputValue} */
-                    /*   onBlur={handleInputValue} */
-                    name="Password"
-                    label="Password"
-                    fullWidth
-                    autoComplete
-                    required
-                />
-                <Button
-                    variant="contained"
-                    type="submit"
-                >
-                    Log in
-                </Button>
-                <Typography>Don't have an account yet? <Link href="/register">Create account.</Link></Typography>
+                <Typography sx={header} variant='h3'>Log in</Typography>
+                <Box sx={loginSecondBox}>
+                    <TextField
+                        /* onChange={handleInputValue} */
+                        /*   onBlur={handleInputValue} */
+                        name="email"
+                        label="Email"
+                        fullWidth
+                        autoComplete
+                        required
+                    />
+                    <TextField
+                        /* onChange={handleInputValue} */
+                        /*   onBlur={handleInputValue} */
+                        name="password"
+                        label="Password"
+                        fullWidth
+                        autoComplete
+                        required
+                    />
+                    <Button
+                        variant="contained"
+                        type="submit"
+                    >
+                        Log in
+                    </Button>
+                    <Typography>Don't have an account? <Link href="/register">Create account.</Link></Typography>
+                </Box>
             </Box>
         </form>
     )
 }
 
 export default Login;
+
+const header = {
+    fontSize: { xs: '36px', sm: '64px', md: '100px' },
+    fontWeight: 'bold',
+    color: '#1F271B'
+}
 
 const loginForm = {
     display: 'flex',
@@ -49,6 +58,14 @@ const loginForm = {
 const loginMainBox = {
     display: 'flex',
     padding: '50px 0',
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: '3rem',
+}
+
+const loginSecondBox = {
+    width: { xs: '300px', lg: 'max-content' },
+    display: 'flex',
     alignItems: 'center',
     flexDirection: 'column',
     gap: '3rem',
--- src/components/pages/user/Register.jsx
+++ src/components/pages/user/Register.jsx
@@ -8,55 +8,64 @@ function Register() {
             autoComplete="off"
             /* onSubmit={handleSubmit} */>
             <Box sx={registerMainBox}>
-                <TextField
-                    /* onChange={handleInputValue} */
-                    /*   onBlur={handleInputValue} */
-                    name="email"
-                    label="Email"
-                    fullWidth
-                    autoComplete
-                    required
-                />
-                <TextField
-                    /* onChange={handleInputValue} */
-                    /*   onBlur={handleInputValue} */
-                    name="fname"
-                    label="First name"
-                    fullWidth
-                    autoComplete
-                    required
-                />
-                <TextField
-                    /* onChange={handleInputValue} */
-                    /*   onBlur={handleInputValue} */
-                    name="lname"
-                    label="Last name"
-                    fullWidth
-                    autoComplete
-                    required
-                />
-                <TextField
-                    /* onChange={handleInputValue} */
-                    /*   onBlur={handleInputValue} */
-                    name="selPassword"
-                    label="Select password"
-                    fullWidth
-                    autoComplete
-                    required
-                />
-                <Button
-                    variant="contained"
-                    type="submit"
-                >
-                    Register
-                </Button>
-                <Typography>Already have an account? <Link href="/login">Log in to your account.</Link></Typography>
+                <Typography sx={header} variant='h3'>Create Account</Typography>
+                <Box sx={registerSecondBox}>
+                    <TextField
+                        /* onChange={handleInputValue} */
+                        /*   onBlur={handleInputValue} */
+                        name="email"
+                        label="Email"
+                        fullWidth
+                        autoComplete
+                        required
+                    />
+                    <TextField
+                        /* onChange={handleInputValue} */
+                        /*   onBlur={handleInputValue} */
+                        name="fname"
+                        label="First name"
+                        fullWidth
+                        autoComplete
+                        required
+                    />
+                    <TextField
+                        /* onChange={handleInputValue} */
+                        /*   onBlur={handleInputValue} */
+                        name="lname"
+                        label="Last name"
+                        fullWidth
+                        autoComplete
+                        required
+                    />
+                    <TextField
+                        /* onChange={handleInputValue} */
+                        /*   onBlur={handleInputValue} */
+                        name="selPassword"
+                        label="Select password"
+                        fullWidth
+                        autoComplete
+                        required
+                    />
+                    <Button
+                        variant="contained"
+                        type="submit"
+                    >
+                        Register
+                    </Button>
+                    <Typography>Already have an account? <Link href="/login">Log in to your account.</Link></Typography>
+                </Box>
             </Box>
         </form>
     )
 }
 
 export default Register;
+
+const header = {
+    fontSize: { xs: '36px', sm: '64px', md: '100px' },
+    fontWeight: 'bold',
+    color: '#1F271B'
+}
 
 const registerForm = {
     display: 'flex',
@@ -67,6 +76,14 @@ const registerForm = {
 const registerMainBox = {
     display: 'flex',
     padding: '50px 0',
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: '3rem',
+}
+
+const registerSecondBox = {
+    width: {xs: '300px', lg:'max-content'},
+    display: 'flex',
     alignItems: 'center',
     flexDirection: 'column',
     gap: '3rem',
