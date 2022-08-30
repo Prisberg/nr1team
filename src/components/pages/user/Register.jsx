21e0d287776ec7f0e6d4ea02b8cea52e5cabadc9
Prisberg <fille.risberg@live.se>
2022-08-30 19:40:45 +0200

    register saves to localhost

--- src/components/pages/user/Register.jsx
+++ src/components/pages/user/Register.jsx
@@ -1,49 +1,74 @@
 import { Box, Button, Link, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import useLocalStorage from "../../../utils/Hook";
+import { useNavigate } from "react-router-dom";
+import { usePortfolioContext } from "../../../utils/PortfolioContext";
 
 function Register() {
+    const { extractLocalStorage } = usePortfolioContext();
+    const navigate = useNavigate();
+
+    const [_email, setEmail] = useLocalStorage("email", "");
+    const [_fName, setFName] = useLocalStorage("fName", "");
+    const [_lName, setLName] = useLocalStorage("lName", "");
+    const [_password, setPassword] = useLocalStorage("password", "");
+
+    const [stateEmail, setStateEmail] = useState("");
+    const [stateFName, setStateFName] = useState("");
+    const [stateLName, setStateLName] = useState("");
+    const [statePassword, setStatePassword] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('submitted');
+        setEmail(stateEmail);
+        setFName(stateFName);
+        setLName(stateLName);
+        setPassword(statePassword);
+
+        extractLocalStorage();
+        navigate('/login')
+        
+        //display created account confirmation
+    }
 
     return (
         <form
             style={registerForm}
-            autoComplete="off"
-            /* onSubmit={handleSubmit} */>
+            onSubmit={(e) => handleSubmit(e)}>
             <Box sx={registerMainBox}>
                 <Typography sx={header} variant='h3'>Create Account</Typography>
                 <Box sx={registerSecondBox}>
                     <TextField
-                        /* onChange={handleInputValue} */
-                        /*   onBlur={handleInputValue} */
+                        onChange={(e) => setStateEmail(e.target.value)}
                         name="email"
                         label="Email"
+                        value={stateEmail}
                         fullWidth
-                        autoComplete
                         required
                     />
                     <TextField
-                        /* onChange={handleInputValue} */
-                        /*   onBlur={handleInputValue} */
+                        onChange={(e) => setStateFName(e.target.value)}
                         name="fname"
                         label="First name"
+                        value={stateFName}
                         fullWidth
-                        autoComplete
                         required
                     />
                     <TextField
-                        /* onChange={handleInputValue} */
-                        /*   onBlur={handleInputValue} */
+                        onChange={(e) => setStateLName(e.target.value)}
                         name="lname"
                         label="Last name"
+                        value={stateLName}
                         fullWidth
-                        autoComplete
                         required
                     />
                     <TextField
-                        /* onChange={handleInputValue} */
-                        /*   onBlur={handleInputValue} */
+                        onChange={(e) => setStatePassword(e.target.value)}
                         name="selPassword"
                         label="Select password"
+                        value={statePassword}
                         fullWidth
-                        autoComplete
                         required
                     />
                     <Button
@@ -82,7 +107,7 @@ const registerMainBox = {
 }
 
 const registerSecondBox = {
-    width: {xs: '300px', lg:'max-content'},
+    width: { xs: '300px', lg: 'max-content' },
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'column',
