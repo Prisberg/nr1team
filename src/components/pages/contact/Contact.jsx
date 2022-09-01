b2209d7580247e3a3da9a1d9fc0e3256e3963f9d
rchtect <rchtectdev@gmail.com>
2022-09-01 15:05:22 +0200

    misc fix

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,16 +1,14 @@
-import { React, BrowserRouter, Routes, Route } from "react";
+import React from "react";
 import { ContactForm } from "./ContactForm";
 import "./contact.css";
-
-
 function Contact() {
   return (
-      <div className="contact-main">
-        <div className="contact-bg">
-          <h1 className="contact-title">Contact us</h1>
-          <ContactForm />
-        </div>
+    <div className="contact-main">
+      <div className="contact-bg">
+        <h1 className="contact-title">Contact us</h1>
+        <ContactForm/>
       </div>
+    </div>
   );
 }
 
