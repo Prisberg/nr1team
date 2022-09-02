c8a913b99a5262df1c14b80c6cc1c501c19f6b9a
elinarnten <elin.arnten@hotmail.com>
2022-09-02 10:38:24 +0200

    size the map

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -32,8 +32,9 @@ function Footer() {
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.198258585644!2d11.990865735479428!3d57.70999836278882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff30de9cbee3d%3A0xbb3f902b63916df6!2sMedieinstitutet!5e0!3m2!1ssv!2sse!4v1662019796756!5m2!1ssv!2sse"
             width="600"
-            maxwidth="auto"
-            height="auto"
+            maxwidth="100%"
+            height="400px"
+            minheight="auto"
             style={mapstyling}
             allowfullscreen="true"
             loading="lazy"
