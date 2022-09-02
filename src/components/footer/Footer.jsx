6d9c56fc3e739d245e7786b8fe1e932db9d1d086
elinarnten <elin.arnten@hotmail.com>
2022-09-02 08:45:52 +0200

    change the styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material";
-import Map from "./map.jpg";
 import React from "react";
 import CopyrightIcon from "@mui/icons-material/Copyright";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -13,7 +12,7 @@ const mapstyling = {
 
 function Footer() {
   return (
-    <Box sx={{ backgroundColor: "#EDE6E3" }}>
+    <Box sx={{ backgroundColor: "#080020" }}>
       <Box
         sx={{
           display: "flex",
@@ -23,74 +22,75 @@ function Footer() {
       >
         <Box
           sx={{
-            backgroundColor: "#F9F7F7",
-            borderRadius: "25px",
             padding: "2rem",
             display: "flex",
             margin: "1rem",
             flexWrap: "wrap",
+            justifyContent: "space-around",
           }}
         >
-          <Box sx={{ margin: "1rem" }}>
-            <Typography variant="h6" sx={{ fontWeight: "bolder" }}>
-              Nr 1 TEAM
-            </Typography>
-            <Typography sx={{ fontWeight: "bolder" }}>
-              For smart web designs
-            </Typography>
-            <Typography>Anders Personsgatan 18</Typography>
-            <Typography>416 64 Göteborg</Typography>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.198258585644!2d11.990865735479428!3d57.70999836278882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff30de9cbee3d%3A0xbb3f902b63916df6!2sMedieinstitutet!5e0!3m2!1ssv!2sse!4v1662019796756!5m2!1ssv!2sse"
+            width="600"
+            height="450"
+            style={mapstyling}
+            allowfullscreen="true"
+            loading="lazy"
+            title="map"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <Box sx={{ p: "2rem" }}>
+            <Box sx={{}}>
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: "bolder", color: "white" }}
+              >
+                Nr 1 TEAM
+              </Typography>
+              <Typography sx={{ fontWeight: "bolder", color: "white" }}>
+                For smart web designs
+              </Typography>
+              <Typography sx={{ color: "white" }}>
+                Anders Personsgatan 18
+              </Typography>
+              <Typography sx={{ color: "white" }}>416 64 Göteborg</Typography>
+            </Box>
+
+            <Box sx={{ pt: "4rem" }}>
+              <Typography variant="h6" sx={{ color: "white" }}>
+                Let's get in touch
+              </Typography>
+              <Typography sx={{ color: "white" }}>
+                Email: nr1team@gmail.com
+              </Typography>
+              <Typography sx={{ color: "white" }}>
+                Phone: 070 666 666
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <LinkedInIcon
+                sx={{ height: "40px", width: "40px", color: "white" }}
+              />
+              <GitHubIcon
+                sx={{ height: "40px", width: "40px", color: "white" }}
+              />
+              <FacebookIcon
+                sx={{ height: "40px", width: "40px", color: "white" }}
+              />
+            </Box>
           </Box>
-          <Box
-            sx={{
-              backgroundImage: `url(${Map})`,
-              backgroundSize: "contain",
-              height: "10rem",
-              width: "15rem",
-            }}
-          ></Box>
         </Box>
-        <Box
-          sx={{
-            backgroundColor: "#F9F7F7",
-            borderRadius: "25px",
-            display: "flex",
-            padding: "2rem",
-            margin: "1rem",
-          }}
-        >
-          <Box sx={{ margin: "1rem" }}>
-            <Typography variant="h6"> Lets get in touch</Typography>
-            <Typography>Email: nr1team@gmail.com</Typography>
-            <Typography>Phone: 070 666 666</Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              ml: "2rem",
-            }}
-          >
-            <LinkedInIcon sx={{ height: "40px", width: "40px" }} />
-            <GitHubIcon sx={{ height: "40px", width: "40px" }} />
-            <FacebookIcon sx={{ height: "40px", width: "40px" }} />
-            <InstagramIcon sx={{ height: "40px", width: "40px" }} />
-          </Box>
-        </Box>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.198258585644!2d11.990865735479428!3d57.70999836278882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff30de9cbee3d%3A0xbb3f902b63916df6!2sMedieinstitutet!5e0!3m2!1ssv!2sse!4v1662019796756!5m2!1ssv!2sse"
-          width="600"
-          height="450"
-          style={mapstyling}
-          allowfullscreen="true"
-          loading="lazy"
-          title="map"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
       </Box>
       <Box sx={{ display: "flex", justifyContent: "center", padding: "1rem" }}>
-        <CopyrightIcon />
-        <Typography sx={{ fontWeight: "50", pl: "5px" }}>NR 1 TEAM</Typography>
+        <CopyrightIcon sx={{ color: "white" }} />
+        <Typography sx={{ fontWeight: "50", pl: "5px", color: "white" }}>
+          NR 1 TEAM
+        </Typography>
       </Box>
     </Box>
   );
