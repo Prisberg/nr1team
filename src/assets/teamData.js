31ccb72535ebfd3e27357a7fa11f3441ac32cbd1
elinarnten <elin.arnten@hotmail.com>
2022-09-07 15:25:25 +0200

    about elin

--- src/assets/teamData.js
+++ src/assets/teamData.js
@@ -19,10 +19,8 @@ export const team = [
   {
     name: "Elin Arntén",
     image: `${elin}`,
-    about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-       Duisaute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    about: `Hello my name is Elin Arntén and I am Front End developer student,
+with a background in testing. I was a part of Elon Musks spaceX testing. Now I'm back on the ground and because the space gave me anxiety. On my sparetime I like to go hiking in beautiful nature environment and I also like to crochet.`,
     github: "https://github.com/elinarnten",
     linkedin: "https://www.linkedin.com/in/elin-arnten/",
   },
